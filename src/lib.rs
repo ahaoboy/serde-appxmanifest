763b01377ff653069fc6b0a31804ee1e99228a33
ahaoboy <504595380@qq.com>
2025-07-12 10:14:31 +0800

    add pub

--- src/lib.rs
+++ src/lib.rs
@@ -4,169 +4,169 @@ use serde::{Deserialize, Serialize};
 #[serde(rename = "Package")]
 pub struct Package {
     #[serde(rename = "Identity")]
-    identity: Identity,
+    pub identity: Identity,
     #[serde(rename = "Properties")]
-    properties: Properties,
+    pub properties: Properties,
     #[serde(rename = "Dependencies")]
-    dependencies: Dependencies,
+    pub dependencies: Dependencies,
     // FIXME: duplicate field `rescap:Capability`
     // #[serde(rename = "Capabilities")]
     // capabilities: Vec<Capabilities>,
     #[serde(rename = "Applications")]
-    applications: Applications,
+    pub applications: Applications,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Identity {
     #[serde(rename = "@Name")]
-    name: String,
+    pub name: String,
     #[serde(rename = "@ProcessorArchitecture")]
-    processor_architecture: Option<String>,
+    pub processor_architecture: Option<String>,
     #[serde(rename = "@Publisher")]
-    publisher: String,
+    pub publisher: String,
     #[serde(rename = "@Version")]
-    version: String,
+    pub version: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Properties {
     #[serde(rename = "DisplayName")]
-    display_name: String,
+    pub display_name: String,
     #[serde(rename = "PublisherDisplayName")]
-    publisher_display_name: String,
+    pub publisher_display_name: String,
     #[serde(rename = "Logo")]
-    logo: String,
+    pub logo: String,
     #[serde(rename = "uap10:AllowExternalContent")]
-    allow_external_content: Option<bool>,
+    pub allow_external_content: Option<bool>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Dependencies {
     #[serde(rename = "TargetDeviceFamily")]
-    target_device_family: Vec<TargetDeviceFamily>,
+    pub target_device_family: Vec<TargetDeviceFamily>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TargetDeviceFamily {
     #[serde(rename = "@Name")]
-    name: String,
+    pub name: String,
     #[serde(rename = "@MinVersion")]
-    min_version: String,
+    pub min_version: String,
     #[serde(rename = "@MaxVersionTested")]
-    max_version_tested: String,
+    pub max_version_tested: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Capabilities {
     #[serde(rename = "rescap:Capability")]
-    capability: Vec<Capability>,
+    pub capability: Vec<Capability>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Capability {
     #[serde(rename = "@Name")]
-    name: String,
+    pub name: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Applications {
     #[serde(rename = "Application")]
-    application: Vec<Application>,
+    pub application: Vec<Application>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Application {
     #[serde(rename = "@Id")]
-    id: String,
+    pub id: String,
     #[serde(rename = "uap10:TrustLevel")]
-    trust_level: Option<String>,
+    pub trust_level: Option<String>,
     #[serde(rename = "uap10:RuntimeBehavior")]
-    runtime_behavior: Option<String>,
+    pub runtime_behavior: Option<String>,
     #[serde(rename = "uap:VisualElements")]
-    visual_elements: VisualElements,
+    pub visual_elements: VisualElements,
     #[serde(rename = "Extensions")]
-    extensions: Option<Extensions>,
+    pub extensions: Option<Extensions>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct VisualElements {
     #[serde(rename = "@AppListEntry")]
-    app_list_entry: Option<String>,
+    pub app_list_entry: Option<String>,
     #[serde(rename = "@DisplayName")]
-    display_name: String,
+    pub display_name: String,
     #[serde(rename = "@Description")]
-    description: String,
+    pub description: String,
     #[serde(rename = "@BackgroundColor")]
-    background_color: String,
+    pub background_color: String,
     #[serde(rename = "@Square150x150Logo")]
-    square_150x150_logo: String,
+    pub square_150x150_logo: String,
     #[serde(rename = "@Square44x44Logo")]
-    square_44x44_logo: String,
+    pub square_44x44_logo: String,
 }
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Extensions {
     #[serde(rename = "desktop4:Extension")]
-    desktop_extension: Option<Vec<DesktopExtension>>,
+    pub desktop_extension: Option<Vec<DesktopExtension>>,
     #[serde(rename = "com:Extension")]
-    com_extension: Option<Vec<ComExtension>>,
+    pub com_extension: Option<Vec<ComExtension>>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct DesktopExtension {
+pub struct DesktopExtension {
     #[serde(rename = "desktop4:FileExplorerContextMenus")]
-    file_explorer_context_menus: FileExplorerContextMenus,
+    pub file_explorer_context_menus: FileExplorerContextMenus,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct FileExplorerContextMenus {
+pub struct FileExplorerContextMenus {
     #[serde(rename = "desktop5:ItemType")]
-    item_type: Vec<ItemType>,
+    pub item_type: Vec<ItemType>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct ItemType {
+pub struct ItemType {
     #[serde(rename = "@Type")]
-    ty: String,
+    pub ty: String,
     #[serde(rename = "desktop5:Verb")]
-    verb: Verb,
+    pub verb: Verb,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Verb {
+pub struct Verb {
     #[serde(rename = "@Id")]
-    id: String,
+    pub id: String,
     #[serde(rename = "@Clsid")]
-    clsid: String,
+    pub clsid: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct ComExtension {
+pub struct ComExtension {
     #[serde(rename = "com:ComServer")]
-    com_server: Option<ComServer>,
+    pub com_server: Option<ComServer>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct ComServer {
+pub struct ComServer {
     #[serde(rename = "com:SurrogateServer")]
-    surrogate_server: Option<SurrogateServer>,
+    pub surrogate_server: Option<SurrogateServer>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct SurrogateServer {
+pub struct SurrogateServer {
     #[serde(rename = "@DisplayName")]
-    display_name: String,
+    pub display_name: String,
     #[serde(rename = "com:Class")]
-    com_class: Vec<ComClass>,
+    pub com_class: Vec<ComClass>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct ComClass {
+pub struct ComClass {
     #[serde(rename = "@Id")]
-    id: String,
+    pub id: String,
     #[serde(rename = "@Path")]
-    path: String,
+    pub path: String,
     #[serde(rename = "@ThreadingModel")]
-    threading_model: String,
+    pub threading_model: String,
 }
 #[cfg(test)]
 mod test {
